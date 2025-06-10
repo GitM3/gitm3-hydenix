5f40020c7f3fd9caa77a786cba7da52fbd6f01c8
zander <zander@polsons.info>
2025-06-10 14:57:03 +0200

    Config update: 2025-06-10 14:57:03

--- modules/hm/programs.nix
+++ modules/hm/programs.nix
@@ -1,13 +1,20 @@
 { config, lib, pkgs, ... }:
 {
   home.packages = with pkgs; [
-    anki-bin 
+    # normal
+    anki-bin # Addons: 1344485230  2055492159  312734862
+    goldendict-ng
     obsidian
+
+    # Shell
     bat
     zellij
+    zoxide
     lazygit
     lazydocker
     git-graph
+
+    # utils
     zathura
     adobe-reader
 
