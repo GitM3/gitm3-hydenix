3d713d2c402d47fc8f791fe739d7de96a3ee0ccc
zander <zander@polsons.info>
2025-07-10 13:50:39 +0200

    Custom yazi plugins

--- modules/hm/yazi.nix
+++ modules/hm/yazi.nix
@@ -1,9 +1,15 @@
 {pkgs, ...}: let
 	yazi-plugins = pkgs.fetchFromGitHub {
-		owner = "yazi-rs";
-		repo = "plugins";
-    rev = "63f9650e522336e0010261dcd0ffb0bf114cf912";
-    hash = "sha256-ZCLJ6BjMAj64/zM606qxnmzl2la4dvO/F5QFicBEYfU=";
+	owner = "yazi-rs";
+	repo = "plugins";
+	rev = "63f9650e522336e0010261dcd0ffb0bf114cf912";
+	hash = "sha256-ZCLJ6BjMAj64/zM606qxnmzl2la4dvO/F5QFicBEYfU=";
+	};
+	yazi-bookmark = pkgs.fetchFromGitHub {
+	  owner="dedukun";
+	  repo="bookmarks.yazi";
+	  rev="9ef1254d8afe88aba21cd56a186f4485dd532ab8";
+	  hash="sha256-GQFBRB2aQqmmuKZ0BpcCAC4r0JFKqIANZNhUC98SlwY=";
 	};
 in {
 	programs.yazi = {
@@ -12,7 +18,7 @@ in {
 		shellWrapperName = "y";
 
 		settings = {
-			manager = {
+			mgr = {
 				show_hidden = true;
 			};
 			preview = {
@@ -26,12 +32,28 @@ in {
 			diff = "${yazi-plugins}/diff.yazi";
 			git = "${yazi-plugins}/git.yazi";
 			toggle-pane = "${yazi-plugins}/toggle-pane.yazi";
-			bookmarks = "${yazi-plugins}/bookmarks";
+			bookmarks   = "${yazi-bookmark}";
 		};
 
 		initLua = ''
-		'';
-
+			require("bookmarks"):setup({
+			last_directory = { enable = true, persist = false, mode="jump" },
+			persist = "vim", --Capital letters are saved persistently.
+			desc_format = "full",
+			file_pick_mode = "parent",
+			custom_desc_input = false,
+			show_keys = true,
+			notify = {
+				enable = true,
+				timeout = 1,
+				message = {
+					new = "New bookmark '<key>' -> '<folder>'",
+					delete = "Deleted bookmark in '<key>'",
+					delete_all = "Deleted all bookmarks",
+					},
+				},
+			})
+			'';
 		keymap = {
 			mgr.prepend_keymap = [
 				{
@@ -48,6 +70,26 @@ in {
 					on = ["c" "m"];
 					run = "plugin chmod";
 					desc = "Chmod on selected files";
+				}
+				{
+				on = [ "m" ];
+				run = "plugin bookmarks save";
+				desc = "Save current position as a bookmark";
+				}
+				{
+				on = [ "'" ];
+				run = "plugin bookmarks jump";
+				desc = "Jump to a bookmark";
+				}
+				{
+				on = [ "b" "d" ];
+				run = "plugin bookmarks delete";
+				desc = "Delete a bookmark";
+				}
+				{
+				on = [ "b" "D" ];
+				run = "plugin bookmarks delete_all";
+				desc = "Delete all bookmarks";
 				}
 			];
 		};
