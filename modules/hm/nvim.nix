6499b7807556e724473e5457d7d35bc1dcd94e85
zander <zander@polsons.info>
2025-08-06 18:32:16 +0200

    Vim window nav remap

--- modules/hm/nvim.nix
+++ modules/hm/nvim.nix
@@ -1,6 +1,25 @@
-{pkgs,inputs, ...}:{
+{ pkgs, inputs, ... }:
+{
   home.packages = [
-       inputs.khanelivim.packages.x86_64-linux.default
-       pkgs.vim  
-];
+    (
+      let
+        baseConfig = inputs.khanelivim.nixvimConfigurations.x86_64-linux.khanelivim;
+        extendedConfig = baseConfig.extendModules {
+          modules = [
+            {
+              extraConfigLua = ''
+                -- Window navigation with Ctrl+hjkl
+                vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Go to left window' })
+                vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Go to bottom window' })
+                vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Go to top window' })
+                vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Go to right window' })
+              '';
+            }
+          ];
+        };
+      in
+      extendedConfig.config.build.package
+    )
+    pkgs.vim
+  ];
 }
