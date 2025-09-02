2e384fd342996404116ccbd5b60a02d64b2e2b4b
GitM3 <zander@polsons.info>
2025-09-02 10:13:54 +0200

    Added nord theme for fcitx

--- modules/hm/keybinds.nix
+++ modules/hm/keybinds.nix
@@ -1,8 +1,16 @@
 {
   lib,
+  pkgs,
   ...
 }:
-
+let
+  fcitx-skin = pkgs.fetchFromGitHub {
+    owner = "Yucklys";
+    repo = "fcitx-nord-skin";
+    rev = "4ae8525024be78d5fb9b9589bac7ff78ab9c4b34";
+    sha256 = "sha256-K6qy09XqnotglMRdd4H2Na6D8QgikY11/PsZKb41dVI=";
+  };
+in
 {
   home.file = {
 
@@ -18,6 +26,7 @@
     ".config/hypr/keybindings.conf" = lib.mkForce {
       source = ../../resources/config/keybinds.conf;
     };
+    ".config/fcitx/skin".source = fcitx-skin;
     # ".config/hyde/themes/Decay Green/wallpapers/cat.png" = {
     #   source = ../../resources/wallpapers/cat.png;
     # };
